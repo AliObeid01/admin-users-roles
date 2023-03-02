6a0391100477dfc7bc0df6b6febd10b855cf342b
unknown <aliiobeid01@gmail.com>
2023-03-02 11:42:03 +0200

    backend:implement jwt auth to the project

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Relations\belongsToMany;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -24,6 +24,14 @@ class User extends Authenticatable
         'gender',
         'blood_type',
     ];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
 
     public function addCertificate(): belongsToMany
     {
