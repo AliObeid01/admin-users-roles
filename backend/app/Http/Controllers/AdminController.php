d28e77319962343426619f5aded387bfd83de166
unknown <aliiobeid01@gmail.com>
2023-03-02 12:24:46 +0200

    backend:create admin login and logout functions in the adminController

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Admin;
+use Validator;
 
 class AdminController extends Controller
 {
-    //
+
+    //login function with validator on the admin input
+    //return the created token
+    public function Adminlogin(Request $request){
+
+    	$validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Please fill all fields'], 401);
+        }
+        if (! $token = auth()->attempt($validator->validated())) {
+            return response()->json(['message' => 'Incorrect email or pasword'], 401);
+        }
+        
+        return $this->respondWithToken($token);
+    }
+
+    //logout function to delete the token and logout from the system
+    //return logout message
+    public function logout() {
+        auth()->logout();
+        return response()->json(['message' => 'admin successfully signed out']);
+    }
+
+    //function to return the admin info with the created token
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'admin'=>auth()->admin()
+        ]);
+    }
+
 }
