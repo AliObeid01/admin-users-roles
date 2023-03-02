64b9db7b96dae99d41d5a81c7fc8a7bbace5cb32
unknown <aliiobeid01@gmail.com>
2023-03-02 12:29:08 +0200

    backend:add api route for login and logout with a middleware group

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,14 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::group(["prefix"=> "v1"], function(){
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
+    Route::post('/Admin', [AdminController::class, 'Adminlogin']);
+
 });
