602d92c45c91702094c204df732b15b9b6d43242
unknown <aliiobeid01@gmail.com>
2023-03-02 10:49:44 +0200

    backend:create users model

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table-> string('name',45);
+            $table-> string('email',45)->unique();
+            $table-> string('password',255);
+            $table-> string('gender',45);
+            $table-> string('blood_type',45);
+            $table-> integer ('status')->default(0);
             $table->timestamps();
         });
     }
