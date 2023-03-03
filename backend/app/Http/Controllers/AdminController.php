e2954f09d4075db27db28535ec434116cca4b7b6
unknown <aliiobeid01@gmail.com>
2023-03-04 00:12:31 +0200

    backend:create addCertificate function in AdminController

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Certificates;
 use Validator;
 
 class AdminController extends Controller
@@ -20,7 +21,24 @@ class AdminController extends Controller
           "data" => $users
        ]);
     }
+ 
+    //addCertificate function to add a certificate
+    //return the certificate information
+    public function addCertificate(Request $request) {
 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|',
+        ]);
+        if($validator->stopOnFirstFailure()->fails()){
+            return response()->json($validator->errors(), 401);
+        }
+        $certificate = Certificates::create($validator->validated());
+                    
+        return response()->json([
+            'message' => 'Certrificate successfully added',
+            'user' => $user
+        ], 201);
+     }
 
 
 }
